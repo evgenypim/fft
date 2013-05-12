bf90c8be4aeee98c7238631de526d98ecc562c10
aelphy <miha-usv@yandex.ru>
2013-05-12 15:40:08 +0600

    rewrite fft

--- draw.rb
+++ draw.rb
@@ -2,14 +2,16 @@
 require 'gnuplot'
 
 class Draw
+  include FFT
+
   Gnuplot.open do |gp|
     Gnuplot::Plot.new( gp ) do |plot|
-      plot.xrange "[0:100]"
+      plot.xrange "[#{FFT::ARG_MIN}:#{FFT::ARG_MAX}]"
       plot.title  "Спектр сигнала"
       plot.ylabel "Частота"
       plot.xlabel "Спектральная плотность"
       
-      plot.data << Gnuplot::DataSet.new( [discret_signal, t] ) do |ds|
+      plot.data << Gnuplot::DataSet.new([FFT.discret_signal, FFT.t]) do |ds|
         ds.with = "linespoints"
         ds.notitle
       end
