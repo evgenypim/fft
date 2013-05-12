1db9731e8acf4006b750de2d852d37b7bf835581
aelphy <miha-usv@yandex.ru>
2013-05-12 15:40:08 +0600

    rewrite fft

--- fft.rb
+++ fft.rb
@@ -1,10 +1,13 @@
 # coding: UTF-8
+require "function"
 
 module FFT
   F_signal = 5
   F_discret = 10
   W_signal = 2 * F_signal * Math::PI
   W_discret = 2 * F_discret * Math::PI
+  ARG_MIN = 0
+  ARG_MAX = 100
 
    discret_signal = []
    discret_spectr = []
@@ -17,5 +20,21 @@ module FFT
          x << time
          #f << i * FD
          #ft << y[i] * Complex(Math.cos(fr[i]*i), Math.sin(fr[i]*i)).abs
+   end
+
+   def self.discret_signal
+     discret_signal
+   end
+
+   def self.discret_spectr
+     discret_spectr 
+   end
+
+   def self.time
+     t
+   end
+
+   def self.freq
+     f
    end
 end
