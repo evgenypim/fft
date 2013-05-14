4d3c6eb4aa22c1e8b57cd33484a963321c17e8e3
Evgeny Pim <evgeny.pim@gmail.com>
2013-05-14 18:51:59 +0600

    new signals

--- lib/fft/test_signal.rb
+++ lib/fft/test_signal.rb
@@ -2,7 +2,20 @@
 require 'fft/function'
 
 class TestSignal < Function
-  def self.cos_signal(ampl = 1, phase = 0, f_division = F_DIVISION_COS)
+  def self.cos_signal(ampl = 1, phase = 0, f_division= F_DIVISION_COS)
     self.new { |n| ampl * Math.cos(2 * Math::PI * n / f_division + phase) }
+  end
+
+  def self.sin_signal(ampl = 1, phase = 0, f_division = F_DIVISION_COS)
+    self.new { |n| ampl * Math.sin(2 * Math::PI * n / f_division + phase) }
+  end
+
+  def self.rect_signal(ampl = 1, q = 1 , ti = 1)
+    # q = f_division / (f_disret * ti)
+    ## TODO
+  end
+
+  def self.f_signal(f_division, f_discret)
+    f_discret / f_division
   end
 end
